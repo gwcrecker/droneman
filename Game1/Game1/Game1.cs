8946b7f2ce1890fec1bc77227a5cfe522c752da0
Grant Recker <gwcrecker@gmail.com>
2017-05-17 15:06:50 -0400

    deleted dronePosition for the second time

--- Game1/Game1/Game1.cs
+++ Game1/Game1/Game1.cs
@@ -23,7 +23,7 @@ namespace Game1
             PlayGame,
             EndGame
         }
-        //GamePadState state = GamePadState.MainMenu;
+        //GamePadState state = GamePadState.MainMenu
 
 
         public Game1()
